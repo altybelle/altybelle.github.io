27957470427bc99ff4e91f934a5373e8e3d0748e
altybelle <altamiro.san5@gmail.com>
2026-02-18 18:23:02 -0300

    portfólio improvisado

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function contact(type) {
+    const links = {
+        'instagram': 'https://www.instagram.com/belezia__/',
+        'linkedin': 'https://www.linkedin.com/in/altamiro-belezia/',
+        'github': 'https://github.com/altybelle'
+    }
+
+    window.location.replace(links[type]);
+}
